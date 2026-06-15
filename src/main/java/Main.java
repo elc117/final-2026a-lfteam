d72ccfaeb2cad0d03840f9dcc5aef499a051a180
luisfelipeSI001 <luisfelipe.lfmdl@gmail.com>
2026-06-15 00:14:45 +0000

    pagina inicial pro projeto

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,36 @@
 import io.javalin.Javalin;
+import model.Atleta;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
+
+        System.out.println("INICIOU O MAIN");
+
+        List<Atleta> atletas = new ArrayList<>();
+
+        atletas.add(
+                new Atleta(
+                        1,
+                        "Luis Felipe",
+                        22,
+                        "Branca",
+                        75.0
+                )
+        );
+
+        atletas.add(
+                new Atleta(
+                        2,
+                        "João Silva",
+                        25,
+                        "Azul",
+                        82.5
+                )
+        );
 
         var app = Javalin.create();
 
@@ -10,6 +38,14 @@ public class Main {
             ctx.result("Sistema de Gerenciamento de Equipes de Jiu-Jitsu");
         });
 
+        app.get("/atletas", ctx -> {
+            ctx.json(atletas);
+        });
+
+        System.out.println("VAI INICIAR O JAVALIN");
+
         app.start(7070);
+
+        System.out.println("JAVALIN INICIADO");
     }
 }
--- src/main/java/model/Atleta.java
+++ src/main/java/model/Atleta.java
@@ -1,7 +1,38 @@
+package model;
+
 public class Atleta {
-    private Long id;
+
+    private int id;
     private String nome;
     private int idade;
     private String faixa;
     private double peso;
+
+    public Atleta(int id, String nome, int idade, String faixa, double peso) {
+        this.id = id;
+        this.nome = nome;
+        this.idade = idade;
+        this.faixa = faixa;
+        this.peso = peso;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public String getFaixa() {
+        return faixa;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
 }
